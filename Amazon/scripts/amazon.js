3a6b43bc9008e0c02da0bc38a2b336123ff96406
LuciferUzami <praveentechci2023@gmail.com>
2024-09-08 12:56:42 +0530

    Make cart inter active

--- Amazon/scripts/amazon.js
+++ Amazon/scripts/amazon.js
@@ -50,10 +50,39 @@ products.forEach((productObject, index) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-products-id="${productObject.id}">
             Add to Cart
           </button>
         </div>`
 })
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML
+
+// Add Button event
+document.querySelectorAll('.js-add-to-cart').forEach((addbtn) => {
+  addbtn.addEventListener('click', () => {
+    const productName = addbtn.dataset.productsId
+
+    //Using find() method 
+    let existingProduct = cart.find(item => item.name === productName)
+
+    // Condition
+    if (!existingProduct){
+      cart.push ({
+        name: productName,
+        quantity: 1
+      })  
+    }else {
+      existingProduct.quantity += 1
+    }
+
+    // Add total contenty
+    let cartTotal = 0
+    cart.forEach((item) => {
+      cartTotal += item.quantity
+    })
+
+    // Show on cart logo
+    document.querySelector('.js-cart-quantity').innerText = cartTotal
+  })
+})
