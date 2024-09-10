97a972825ddad089d6a99cfa4a405e98d6817585
LuciferUzami <praveentechci2023@gmail.com>
2024-09-10 10:32:56 +0530

    set Local storage and delete option work properly and save on local storage

--- Amazon/data/cart.js
+++ Amazon/data/cart.js
@@ -1,10 +1,20 @@
-export const cart = [{
-  name: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2
-}, {
-  name: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
-}]
+export let cart = JSON.parse(localStorage.getItem('cart'))
+if (!cart) {
+  cart = [{
+    name: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2
+  }, {
+    name: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  }]
+  
+}
+
+// Set localStorage
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart))
+}
+
 
 export function addTocart(productName) {
   //Using find() method 
@@ -19,7 +29,21 @@ export function addTocart(productName) {
   }else {
     existingProduct.quantity += 1
   }
+  saveToStorage()
 }
 
+
+// Removing product form cart
+export function removeFromCart(deleteProduct) {
+  const newCart = []
+  cart.forEach((cartItem) =>{
+    if (deleteProduct !== cartItem.name) {
+      newCart.push(cartItem)
+    }
+  })
+  cart = newCart
+
+  saveToStorage()
+}
 
 
--- Amazon/scripts/checkout.js
+++ Amazon/scripts/checkout.js
@@ -1,102 +1,115 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 
 let cartSummaryHTML = ''
 
 // loop
-cart.forEach((cartItem) => {
-  const productId = cartItem.name
-
-  let matchingId;
-
-  products.forEach((productData) => {
-    if (productData.id === productId) {
-      matchingId = productData
-    }
+function cartSlide() {
+  cart.forEach((cartItem, index) => {
+    const productId = cartItem.name
+  
+    let matchingId;
+  
+    products.forEach((productData) => {
+      if (productData.id === productId) {
+        matchingId = productData
+        console.log(matchingId)
+      }
+    });
+  
+    // console.log(matchingId)
+    cartSummaryHTML += `
+    <div class="cart-item-container js-item-id-${matchingId.id}">
+            <div class="delivery-date">
+              Delivery date: Tuesday, June 21
+            </div>
+  
+            <div class="cart-item-details-grid">
+              <img class="product-image"
+                src=${matchingId.image}>
+  
+              <div class="cart-item-details">
+                <div class="product-name">
+                  ${matchingId.names}
+                </div>
+                <div class="product-price">
+                  ₹${matchingId.price}
+                </div>
+                <div class="product-quantity">
+                  <span>
+                    Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+                  </span>
+                  <span class="update-quantity-link link-primary">
+                    Update
+                  </span>
+                  <span class="delete-quantity-link link-primary js-delete-cart" data-product-id = "${matchingId.id}">
+                    Delete
+                  </span>
+                </div>
+              </div>
+  
+              <div class="delivery-options">
+                <div class="delivery-options-title">
+                  Choose a delivery option:
+                </div>
+                <div class="delivery-option">
+                  <input type="radio" checked
+                    class="delivery-option-input"
+                    name="delivery-option-${index}">
+                  <div>
+                    <div class="delivery-option-date">
+                      Tuesday, June 21
+                    </div>
+                    <div class="delivery-option-price">
+                      FREE Shipping
+                    </div>
+                  </div>
+                </div>
+                <div class="delivery-option">
+                  <input type="radio"
+                    class="delivery-option-input"
+                    name="delivery-option-${index}">
+                  <div>
+                    <div class="delivery-option-date">
+                      Wednesday, June 15
+                    </div>
+                    <div class="delivery-option-price">
+                      ₹50 - Shipping
+                    </div>
+                  </div>
+                </div>
+                <div class="delivery-option">
+                  <input type="radio"
+                    class="delivery-option-input"
+                    name="delivery-option-${index}">
+                  <div>
+                    <div class="delivery-option-date">
+                      Monday, June 13
+                    </div>
+                    <div class="delivery-option-price">
+                      ₹100 - Shipping
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+    `
   });
+}
 
-  // console.log(matchingId)
-  cartSummaryHTML += `
-  <div class="cart-item-container">
-          <div class="delivery-date">
-            Delivery date: Tuesday, June 21
-          </div>
+// Call the slide function
+cartSlide()
 
-          <div class="cart-item-details-grid">
-            <img class="product-image"
-              src=${matchingId.image}>
-
-            <div class="cart-item-details">
-              <div class="product-name">
-                ${matchingId.names}
-              </div>
-              <div class="product-price">
-                ₹${matchingId.price}
-              </div>
-              <div class="product-quantity">
-                <span>
-                  Quantity: <span class="quantity-label">${cartItem.quantity}</span>
-                </span>
-                <span class="update-quantity-link link-primary">
-                  Update
-                </span>
-                <span class="delete-quantity-link link-primary">
-                  Delete
-                </span>
-              </div>
-            </div>
-
-            <div class="delivery-options">
-              <div class="delivery-options-title">
-                Choose a delivery option:
-              </div>
-              <div class="delivery-option">
-                <input type="radio" checked
-                  class="delivery-option-input"
-                  name="delivery-option-1">
-                <div>
-                  <div class="delivery-option-date">
-                    Tuesday, June 21
-                  </div>
-                  <div class="delivery-option-price">
-                    FREE Shipping
-                  </div>
-                </div>
-              </div>
-              <div class="delivery-option">
-                <input type="radio"
-                  class="delivery-option-input"
-                  name="delivery-option-1">
-                <div>
-                  <div class="delivery-option-date">
-                    Wednesday, June 15
-                  </div>
-                  <div class="delivery-option-price">
-                    $4.99 - Shipping
-                  </div>
-                </div>
-              </div>
-              <div class="delivery-option">
-                <input type="radio"
-                  class="delivery-option-input"
-                  name="delivery-option-1">
-                <div>
-                  <div class="delivery-option-date">
-                    Monday, June 13
-                  </div>
-                  <div class="delivery-option-price">
-                    $9.99 - Shipping
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-
-        
-
-  `
-});
-
+// Add div html
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML
+
+// for delete 
+document.querySelectorAll('.js-delete-cart').forEach((deleteHTML) =>{
+  deleteHTML.addEventListener('click', () =>{
+    const deleteProduct = deleteHTML.dataset.productId
+    //Call function from cart.js
+    removeFromCart(deleteProduct)
+    document.querySelector(`.js-item-id-${deleteProduct}`).remove()
+   })
+})
